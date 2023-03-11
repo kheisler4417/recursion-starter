7160d0bf79f6c4df336aadf08fbcbee9f6ef2022
kheisler4417 <kheisler4417@gmail.com>
2023-03-11 11:30:34 -0500

    first... changed name in gitHub let's see

--- problems/02-sum-array.js
+++ problems/02-sum-array.js
@@ -10,8 +10,17 @@ sumArray([0, 1, -3]); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
 
-// your code here
-  
+function sumArray(arr) {
+  if (arr.length === 0) {
+    return null;
+  } else if (arr.length === 1) {
+    return arr[0];
+  } else {
+    const lastElement = arr.pop();
+    return lastElement + sumArray(arr);
+  }
+}
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
